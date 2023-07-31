06aab92304d70f98e827cf93df4add7da2765faa
mercysound <adekunleadewolemercy@gmail.com>
2023-07-31 16:59:01 +0100

    done

--- vanilla.js
+++ vanilla.js
@@ -1,37 +1,18 @@
-// const sendMsg = ()=>{
-//   // // alert("work")
-//   // var inputMsg = document.getElementById("inpMsg").value
-//   // document.getElementById("userName").innerHTML += inputMsg
-//   // inputMsg = "";
-  
-//   // document.getElementById("dispMsg").innerHTML += <h1></h1>inputMsg
-// }
-// alert("Welcome")
 var globerUser = "";
 const askName = () => {
   globerUser = prompt("What is your name");
 
-  document.getElementById("userName").innerHTML = `Welcome, ${globerUser}`; 
+  document.getElementById("userName").innerHTML = `Welcome, <span class="text-dark">${globerUser}</span>`; 
 
 }
 askName();
 
 function sendMsg() {
-  // globerUser = yourName
-  // var message = msgInp.value 
-  // document.getElementById("chatArea").innerHTML += "<br/>" + message
-  var message = document.getElementById("msgInp").value 
-  document.getElementById("chatLine").innerHTML = document.getElementById("chatLine").innerHTML + "<br/>" + `<div class="shadow p-2 mx-2" style="color:red; width:20vw;"><span>${globerUser}, said: </span>${message}</div>`
-  document.getElementById("msgInp").value = ""
-  document.getElementById("msgInp").focus()
+  var message = document.getElementById("msgInp").value ;
+  document.getElementById("chatLine").innerHTML = document.getElementById("chatLine").innerHTML + "<br/>" + `<div class="shadow p-2 mx-2 bg-success text-light" style="color:; width:20vw;"><span class="fw-bolder">${globerUser}, </span><span class="fst-normal">said:</span> </span>${message}</div>`
+  document.getElementById("msgInp").value = "";
+  document.getElementById("msgInp").focus();
   message.focus()
    
   
 }
-// how to write to DOM from js
-// var names = "Adewole Adekunle Mercy"
-// document.write(name)
-// is there anyway to modify the content with js
-// var h = document.getElementById("intro")
-//  var newH = h.innerHTML += ": <b>Adewole</b>"
-//  newH.style.color = "red"
