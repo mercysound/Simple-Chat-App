3d76f594c1c9290e3308f260b5f27447be9d52de
mercysound <adekunleadewolemercy@gmail.com>
2023-09-04 19:35:10 +0100

    done

--- vanilla.js
+++ vanilla.js
@@ -1,4 +1,5 @@
 var globerUser = "";
+var Unknown = "unknow User"
 var count = 0;
 const askName = () => {
   if(globerUser = prompt("What is your name")){
@@ -14,11 +15,15 @@ askName();
 
 function sendMsg(){
   count += 1;
-  var message = document.getElementById("msgInp").value ;
-  document.getElementById("chatLine").innerHTML = document.getElementById("chatLine").innerHTML + "<br/>" + `<div class="shadow p-2 mx-2 bg-success text-light" style="color:; width:20vw;"><span class="fw-bolder">${globerUser}, </span><span class="fst-normal">said:</span> </span>${message}</div>`
+  if(msgInp.value == ''){
+    alert("fill the input to continue")
+  }else{
+    var message = document.getElementById("msgInp").value ;
+  document.getElementById("chatLine").innerHTML = document.getElementById("chatLine").innerHTML + "<br/>" + `<div class="shadow p-2 mx-2 bg-success text-light" style="color:; min-width:10%;"><span class="fw-bolder">${globerUser == "" ? "Unknown" : globerUser}, </span><span class="fst-normal">said:</span> </span>${message}</div>`
   document.getElementById("msgInp").value = "";
   document.getElementById('msgInp').focus();
   document.getElementById('msgCount').innerHTML = count;
+  }
   if(count === 0){
     document.getElementById('s').innerHTML = "";
   }else{
@@ -26,6 +31,7 @@ function sendMsg(){
   }
   
 }
+
 // multiplication table
 // for(i = 1; i < 10; i++){
 //   console.log(i);
